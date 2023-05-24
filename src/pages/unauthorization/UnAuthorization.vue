b3a6ac2910f717eb56d35a8bd9b58994f1e7c81c
hoaianh <anhtth@hp-backup.com>
2023-05-24 15:05:27 +0700

    authorization

--- src/pages/unauthorization/UnAuthorization.vue
+++ src/pages/unauthorization/UnAuthorization.vue
@@ -1,3 +1,20 @@
 <template>
     <img src="/img/401.jpg"/>
+
+    <div class="group-button">
+        <button @click="$router.push(PATH.login.url)">Login</button>
+    </div>
+
 </template>
+
+<script lang="ts">
+
+import { PATH } from '../../constants/path'
+export default {
+    data() {
+        return {
+            PATH: PATH
+        }
+    },
+}
+</script>
